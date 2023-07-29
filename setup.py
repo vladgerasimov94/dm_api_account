4f03d0fa97c21c4056dd61de52a4f2ed1c54fff8
vgerasimov <vladgerasimov94@yandex.ru>
2023-07-29 17:23:42 +0300

    up version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ REQUIRES = [
 
 setup(
     name='dm_api_account',
-    version='0.0.1',
+    version='0.0.2',
     packages=['dm_api_account'],
     url='https://github.com/vladgerasimov94/dm_api_account.git',
     license='MIT',
